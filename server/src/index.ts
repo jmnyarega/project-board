aae6c465b7e3e3750b431060002707c249b0faa2
jmnyarega <jmnyarega@gmail.com>
2025-07-29 12:16:05 +0300

    organize folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
-import {ApolloServer} from '@apollo/server';
-import {startStandaloneServer} from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -124,8 +124,8 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const {url} = await startStandaloneServer(server, {
-  listen: {port: 4000},
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
