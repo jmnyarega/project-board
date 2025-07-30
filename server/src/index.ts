92b404bab66b945ffe3805c4cb2834c18c0ce4c1
josiah <jmnyarega@gmail.com>
2025-07-30 13:35:45 +0300

    Refactor: move schema to separate file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,50 +1,8 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import {readFileSync} from "fs";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-type User {
-  name: String
-}
-
-type Card {
-  name: String
-  content: String
-  position: Int
-}
-
-type Message {
-  message: String
-}
-
-type Column {
-  name: String
-  position: Int
-  cards: [Card]
-}
-
-type Board {
-  title: String
-  user: User
-  columns: [Column]
-}
-
-type Mutation {
-  createBoard(title: String!): Board
-  updateBoard(id: ID!, title: String!): Board
-  deleteBoard(id: ID!): Message
-}
-
-
-type Query {
-    board: [Board]
-    column: [Column]
-    card: [Card]
-}
-`;
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+const typeDefs = readFileSync("./schema.graphql", {encoding: "utf-8"});
 
 const user = {
   name: "josiah"
@@ -124,8 +82,8 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+const {url} = await startStandaloneServer(server, {
+  listen: {port: 4000},
 });
 
 console.log(`🚀  Server ready at: ${url}`);
